b1c5ff59eeec6997a0345bfbdad602ddf4295b7c
monkeycs60 <clement.serizay@gmail.com>
2022-04-11 16:15:55 +0200

    html css début js

---
+++ 1. Quizz/app.js
@@ -0,0 +1,6 @@
+let reponse1 = document.getElementById("btn3").value;
+let reponse2 = document.getElementById("btn4").value;
+let reponse3 = document.getElementById("btn8").value;
+let reponse4 = document.getElementById("btn10").value;
+let reponse5 = document.getElementById("btn15").value;
+
